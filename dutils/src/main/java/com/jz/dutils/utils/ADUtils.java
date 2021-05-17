b06165fe99617b9c949e7a3a88ba2fb896355f53
jiuyu0319 <jiuyu0319@163.com>
2021-05-17 13:57:23 +0800

    修改广告1.0.3.

--- dutils/src/main/java/com/jz/dutils/utils/ADUtils.java
+++ dutils/src/main/java/com/jz/dutils/utils/ADUtils.java
@@ -111,7 +111,6 @@ public class ADUtils {
                     }
                 }
 
-
                 if (adStatus==13){  // 如果当前广告商广告idstatus不可用 切另一个广告
                     for (ADBean.DataBean.AdtabBean bean : posadinfo) {
                         if (bean.getStatus()==1) {
